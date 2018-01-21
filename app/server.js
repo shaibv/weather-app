34bc67486388f9c32d1f8f3ad3dfd523d847e747
Shai Ben Valid <shai@slatescience.com>
2018-01-21 16:19:46 +0200

    serving index.html

--- app/server.js
+++ app/server.js
@@ -1,5 +1,12 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var fs = require("fs");
+
 connect().use(serveStatic(__dirname)).listen(9000, function(){
     console.log('Server running on 9000...');
+    fs.readFile("index.html", function(err, data){
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+    })
 });
